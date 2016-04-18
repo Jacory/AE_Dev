fc3cc2bccf6198fa910d34483a8e2be8d8a6b6a9
hengyu123 <hengyu.hengyu@163.com>
2016-04-18 17:08:20 +0800

    Tian (#1)
    * 添加属性表选择列功能
    
    * 完成属性表列全选
    
    * 属性表关联地图界面
    
    * 完善图形属性关联
    
    * 完善单元格选择，地图定位显示

---
+++ AE_Dev_J/Form/temp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace AE_Dev_J.Form
+{
+    public partial class temp : DevExpress.XtraEditors.XtraForm
+    {
+        public temp()
+        {
+            InitializeComponent();
+        }
+    }
+}
